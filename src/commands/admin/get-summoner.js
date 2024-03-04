3904babd19ebce0330bb8cfdc4527accadc3a2de
Brendan <brendan612@gmail.com>
2024-03-04 00:21:18 -0800

    Update get-summoner.js

--- src/commands/admin/get-summoner.js
+++ src/commands/admin/get-summoner.js
@@ -29,7 +29,7 @@ module.exports = {
 			\r\nPUUID: ${summoner.puuid}
 			\r\nRevision Date: ${summoner.revisionDate}
 			\r\nProfile Icon ID: ${summoner.profileIconId}\r\n`,
-			ephemeral: false,
+			ephemeral: true,
 		});
 	},
 };
