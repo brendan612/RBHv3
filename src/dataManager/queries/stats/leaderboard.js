4a0fe0645dfb696c2eb06da19c7506bc156f3408
Brendan <brendan612@gmail.com>
2024-03-22 03:11:59 -0700

    Update leaderboard.js

--- src/dataManager/queries/stats/leaderboard.js
+++ src/dataManager/queries/stats/leaderboard.js
@@ -16,7 +16,7 @@ const { Op, literal } = require("sequelize");
  * @param {number} offset
  * @param {string} region
  * @param {number} minimumMatches
- * @returns {<Promise<User[]>} An array of users.
+ * @returns {<Promise<Any[]>} An array of users.
  */
 async function getLeaderboard(
 	game_id,
@@ -26,81 +26,56 @@ async function getLeaderboard(
 	region,
 	minimumMatches = 3
 ) {
-	const leaderboard = await User.findAll({
-		attributes: {
-			include: [
-				"user_id", // Direct attribute from User
-				// Include 'elo_rating' from 'UserEloRatings' through subquery or direct association
-				[
-					sequelize.literal(
-						`(SELECT elo_rating FROM UserEloRatings WHERE UserEloRatings.user_id = User.user_id ORDER BY UserEloRatings.created_at DESC LIMIT 1)`
-					),
-					"elo_rating",
-				],
-				[
-					sequelize.literal(`
-                    (SELECT COUNT(*)
-                     FROM MatchPlayers mp
-                     INNER JOIN Matches m ON mp.match_id = m.match_id
-                     WHERE mp.user_id = User.user_id
-                     AND (${season_id ? `m.season_id = ${season_id}` : "true"}) 
-                     AND mp.elo_change > 0
-                     AND m.end_time > NOW() - INTERVAL 7 DAY )`),
-					"wins",
-				],
-				[
-					sequelize.literal(`
-                    (SELECT COUNT(*)
-                     FROM MatchPlayers mp
-                     INNER JOIN Matches m ON mp.match_id = m.match_id
-                     WHERE mp.user_id = User.user_id
-                     AND (${season_id ? `m.season_id = ${season_id}` : "true"}) 
-                     AND mp.elo_change < 0
-                     AND m.end_time > NOW() - INTERVAL 7 DAY )`),
-					"losses",
-				],
-			],
-		},
-		include: [
-			{
-				model: MatchPlayer,
-				attributes: [],
-				required: true,
-				include: [
-					{
-						model: Match,
-						attributes: [],
-						where: {
-							...(season_id && { season_id: season_id }),
-							end_time: { [Op.ne]: null },
-							game_id: game_id,
-							region: region,
-						},
-					},
-				],
-			},
-			{
-				model: UserEloRating,
-				as: "UserEloRatings", // Ensure this alias matches the association alias
-				attributes: [], // You may not need to explicitly include attributes if they're being used outside the attributes array
-				required: true,
-			},
-		],
-		group: ["User.user_id", "UserEloRatings.elo_rating"],
-		//prettier-ignore
-		having: sequelize.literal(`(SELECT COUNT(*)
-                                FROM MatchPlayers mp
-                                INNER JOIN Matches m ON mp.match_id = m.match_id
-                                WHERE mp.user_id = User.user_id
-                                AND (${season_id ? `m.season_id = ${season_id}`: "true"})
-                                AND m.end_time > NOW() - INTERVAL 7 DAY 
-                                ) >= ${minimumMatches}`),
-		order: [[sequelize.col("UserEloRatings.elo_rating"), "DESC"]], // Corrected order reference
-		limit: limit,
-		offset: offset,
-	});
+	const [results, metadata] = await sequelize.query(
+		`SELECT 
+			U.user_id,
+			U.summoner_name,
+			E.elo_rating,
+			(SELECT COUNT(*) 
+			FROM MatchPlayers MP
+			JOIN Matches M ON MP.match_id = M.match_id
+			WHERE MP.user_id = U.user_id
+			AND M.game_id = ${game_id}
+			AND (${season_id ? `M.season_id = ${season_id}` : "true"})
+			AND M.region = '${region}'
+			AND MP.elo_change > 0
+			AND M.end_time IS NOT NULL) AS wins,
+			(SELECT COUNT(*) 
+			FROM MatchPlayers MP
+			JOIN Matches M ON MP.match_id = M.match_id
+			WHERE MP.user_id = U.user_id
+			AND M.game_id = ${game_id}
+			AND (${season_id ? `M.season_id = ${season_id}` : "true"})
+			AND M.region = '${region}'
+			AND MP.elo_change < 0
+			AND M.end_time IS NOT NULL) AS losses
+		FROM 
+			Users U
+		JOIN 
+			UserEloRatings E ON U.user_id = E.user_id
+		WHERE 
+			EXISTS (
+				SELECT 1
+				FROM MatchPlayers MP
+				JOIN Matches M ON MP.match_id = M.match_id
+				WHERE MP.user_id = U.user_id
+				AND M.game_id = ${game_id}
+				AND (${season_id ? `M.season_id = ${season_id}` : "true"})
+				AND M.region = '${region}'
+				AND M.end_time > NOW() - INTERVAL 7 DAY
+				GROUP BY MP.user_id
+				HAVING COUNT(MP.match_id) >= ${minimumMatches}
+			)
+		ORDER BY 
+			E.elo_rating DESC
+		LIMIT 
+			${offset}, ${limit};
+	`
+	);
 
-	return leaderboard;
+	console.log(results);
+
+	return results;
 }
 
 module.exports = {
