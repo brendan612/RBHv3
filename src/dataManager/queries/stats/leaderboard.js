a8625c4e8d76aa08d857a9ec130ec11ed8904a92
Brendan <brendan612@gmail.com>
2024-03-23 22:47:13 -0700

    Update leaderboard.js

--- src/dataManager/queries/stats/leaderboard.js
+++ src/dataManager/queries/stats/leaderboard.js
@@ -27,75 +27,100 @@ async function getLeaderboard(
 		return cachedLeaderboard;
 	}
 
-	const includeInactiveClause = includeInactive
-		? ""
-		: "AND (E.elo_rating <= 900 OR (E.elo_rating > 900 AND M.end_time > NOW() - INTERVAL 7 DAY))";
-
 	const [results, metadata] = await sequelize.query(
 		`
-		SELECT
+			WITH RecentMatches AS (
+				SELECT
+					MP.user_id,
+					COUNT(MP.match_id) AS recent_match_count,
+					MAX(M.end_time) AS last_active
+				FROM
+					MatchPlayers MP
+				JOIN
+					Matches M ON MP.match_id = M.match_id
+				WHERE
+					M.game_id = ${game_id}
+					AND (${season_id ? `M.season_id = ${season_id}` : "true"})
+					AND M.region = '${region}'
+					AND M.end_time IS NOT NULL
+				GROUP BY
+					MP.user_id
+			),
+			UserElo AS (
+				SELECT
+					U.user_id,
+					U.summoner_name,
+					U.tag_line,
+					E.elo_rating,
+					IFNULL(RM.recent_match_count, 0) AS recent_match_count,
+					RM.last_active
+				FROM
+					Users U
+				JOIN
+					UserEloRatings E ON U.user_id = E.user_id
+				LEFT JOIN
+					RecentMatches RM ON U.user_id = RM.user_id
+				WHERE
+					E.game_id = ${game_id}
+					AND (${season_id ? `E.season_id = ${season_id}` : "true"})
+					AND LENGTH(U.user_id) > 5
+			),
+			UserStats AS (
+				SELECT
+					user_id,
+					summoner_name,
+					tag_line,
+					elo_rating,
+					(
+						SELECT COUNT(*)
+						FROM MatchPlayers MP
+						JOIN Matches M ON MP.match_id = M.match_id
+						WHERE MP.user_id = UE.user_id
+						AND M.game_id = ${game_id}
+						AND (${season_id ? `M.season_id = ${season_id}` : "true"})
+						AND M.region = '${region}'
+						AND MP.elo_change > 0
+					) AS wins,
+					(
+						SELECT COUNT(*)
+						FROM MatchPlayers MP
+						JOIN Matches M ON MP.match_id = M.match_id
+						WHERE MP.user_id = UE.user_id
+						AND M.game_id = ${game_id}
+						AND (${season_id ? `M.season_id = ${season_id}` : "true"})
+						AND M.region = '${region}'
+						AND MP.elo_change < 0
+					) AS losses,
+					UE.recent_match_count,
+					UE.last_active
+				FROM
+					UserElo UE
+			)
+		SELECT DISTINCT
 			user_id,
 			summoner_name,
 			tag_line,
 			elo_rating,
 			wins,
 			losses,
-			RANK() OVER (ORDER BY elo_rating DESC, (wins + losses) DESC) AS rank
-		FROM (
-			SELECT DISTINCT
-				U.user_id,
-				U.summoner_name,
-				U.tag_line,
-				E.elo_rating,
-				(
-					SELECT COUNT(*) 
-					FROM MatchPlayers MP
-					JOIN Matches M ON MP.match_id = M.match_id
-					WHERE MP.user_id = U.user_id
-					AND M.game_id = ${game_id}
-					AND (${season_id ? `M.season_id = ${season_id}` : "true"})
-					AND M.region = '${region}'
-					AND MP.elo_change > 0
-					AND M.end_time IS NOT NULL
-				) AS wins,
-				(
-					SELECT COUNT(*) 
-					FROM MatchPlayers MP
-					JOIN Matches M ON MP.match_id = M.match_id
-					WHERE MP.user_id = U.user_id
-					AND M.game_id = ${game_id}
-					AND (${season_id ? `M.season_id = ${season_id}` : "true"})
-					AND M.region = '${region}'
-					AND MP.elo_change < 0
-					AND M.end_time IS NOT NULL
-				) AS losses
-			FROM 
-				Users U
-			JOIN 
-				UserEloRatings E ON U.user_id = E.user_id
-			WHERE 
-				EXISTS (
-					SELECT 1
-					FROM MatchPlayers MP
-					JOIN Matches M ON MP.match_id = M.match_id
-					WHERE MP.user_id = U.user_id
-					AND M.game_id = ${game_id}
-					AND (${season_id ? `M.season_id = ${season_id}` : "true"})
-					AND M.region = '${region}'
-					${includeInactiveClause}
-					GROUP BY MP.user_id
-					HAVING COUNT(MP.match_id) >= ${minimumMatches}
-				)
-				AND E.game_id = 1
-				AND E.season_id = 28	
-			ORDER BY 
-				E.elo_rating DESC
-		) AS leaderboard
+			IF(elo_rating > 900 AND (last_active < NOW() - INTERVAL 7 DAY OR last_active IS NULL), 'Inactive', 'Active') AS activity_status,
+			RANK() OVER (ORDER BY elo_rating DESC, wins DESC) AS rank
+		FROM
+			UserStats
+		WHERE recent_match_count >= ${minimumMatches}
+		ORDER BY
+			rank;
 	`
 	);
 
+	if (results) {
+		results = results.filter((user) => {
+			return includeInactive || user.activity_status === "Active";
+		});
+	}
+
 	client.cache.set(
-		`leaderboard-${game_id}-${season_id}-${region}-${minimumMatches}`,
+		`leaderboard-${game_id}-${season_id}-${region}-${minimumMatches}-${includeInactive}`,
 		results,
 		"leaderboard"
 	);
